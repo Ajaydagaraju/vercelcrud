0a2f339289b6f7cc00d44db4aa7039770aa4ee5e
ajay <ajaydagar1434@gmail.com>
2024-08-27 22:10:13 +0530

    add suspense

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SignUp from './signup'
-
+import { Suspense } from 'react';
 const SignIn = () => {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <div style={{ margin:'0 auto', textAlign:'center' }} >
     <h1 style={{fontSize:'24px' }} >Add your Details</h1>
         <SignUp/>
     </div>
+    </Suspense>
   )
 }
 
